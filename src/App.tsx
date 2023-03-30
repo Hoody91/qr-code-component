93e0564a0d91639e1bb1b0b5c8e9b95e8d422031
Riley Hood <68184680+Hoody91@users.noreply.github.com>
2023-03-30 17:52:48 +1100

    Try this

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
 	return (
 		<div className="card">
-			<img src="/image-qr-code.png"></img>
+			<img src="/qr-code-component/image-qr-code.png"></img>
 			<h1>Improve your front-end skills by building projects</h1>
 			<p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
 		</div>
